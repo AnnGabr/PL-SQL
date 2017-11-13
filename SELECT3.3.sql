ba900300ed6048ef13e3deacea1257cae491a500
AnnGabr <annabaks1997@gmail.com>
2017-11-13 12:32:02 +0300

    Update SELECT3.3.sql

--- SELECT3.3.sql
+++ SELECT3.3.sql
@@ -1,14 +1,14 @@
 -- Предствить имя каждого сотрудника тфблицы EMP и имя его руководителя:
 -----------------------------------------------------------------------
 SELECT t1.empname || ' works for ' || t2.empname as Emp_Manager
-FROM emp t1, emp t2
-WHERE t1.manager_id = t2.empno;
+     FROM emp t1, emp t2
+     WHERE t1.manager_id = t2.empno;
  
 -- Требуется представить имя каждого сотрудника таблицы EMP (даже сотрудника, которому не назначен руководитель) и имя его руководителя:
 -----------------------------------------------------------------------
 SELECT empname || ' reports to ' || PRIOR empname AS 'Walk top down'  
-START WITH MANAGER_ID IS NULL
-CONNECT BY PRIOR empno = manager_id;
+     START WITH MANAGER_ID IS NULL
+     CONNECT BY PRIOR empno = manager_id;
  
 -- Требуется показать иерархию от CLARK до JOHN KLINTON:
 -----------------------------------------------------------------------
@@ -36,6 +36,6 @@ SELECT lpad(' ',level,'*') || empname as emp_tree
 -- 6. Требуется найти всех служащих, которые явно или  неявно подчиняются ALLEN:
 -----------------------------------------------------------------------
 SELECT empname  
-FROM emp
-START WITH empname = 'ALLEN'
-CONNECT BY PRIOR empno = manager_id ;
+    FROM emp
+    START WITH empname = 'ALLEN'
+    CONNECT BY PRIOR empno = manager_id ;
