bfc1321db8c7d731a08d0e702dedae992e58c86e
AnnGabr <annabaks1997@gmail.com>
2017-11-19 17:46:45 +0300

    1 done

--- DATE3.4.sql
+++ DATE3.4.sql
@@ -1,1 +1,17 @@
+--Используя значения столбца STARTDATE получить дату
+---------------------------------------------------------------------
+--за десять дней до и после приема на работу сотрудника JOHN KLINTON:
+SELECT (STARTDATE - 10) AS BEFORE10DAYS, (STARTDATE + 10) AS AFTER10DAYS FROM CAREER 
+  WHERE CAREER.EMPNO = (SELECT EMPNO FROM EMP 
+                          WHERE LOWER(EMP.EMPNAME) = 'richard martin');
+                          
+--пол года до и после приема на работу сотрудника JOHN KLINTON: 
+SELECT ADD_MONTHS(STARTDATE, -6) AS BEFORE_HALF_Y, ADD_MONTHS(STARTDATE, 6) AS AFTER_HALF_Y FROM CAREER 
+  WHERE CAREER.EMPNO = (SELECT EMPNO FROM EMP 
+                          WHERE LOWER(EMP.EMPNAME) = 'richard martin');
+                          
+--год до и после приема на работу сотрудника JOHN KLINTON:
+SELECT ADD_MONTHS(STARTDATE, -12) AS BEFORE_ONE_Y, ADD_MONTHS(STARTDATE, 12) AS AFTER_ONE_Y FROM CAREER 
+  WHERE CAREER.EMPNO = (SELECT EMPNO FROM EMP 
+                          WHERE LOWER(EMP.EMPNAME) = 'richard martin');
 
