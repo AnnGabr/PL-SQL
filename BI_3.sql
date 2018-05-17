64543dcce381b2db7105a1d0e06f9ef7a33bde6d
AnnGabr <annabaks1997@gmail.com>
2018-05-17 15:32:37 +0300

    2 done

--- BI_3.sql
+++ BI_3.sql
@@ -1,2 +1,8 @@
 1-------------------------------
   SELECT empno, salvalue, RANK() OVER(ORDER BY salvalue) rnk, DENSE_RANK() OVER(ORDER BY salvalue) rnk_dense FROM SALARY;
+
+2-------------------------------
+  SELECT career.jobno, salary.salvalue, RATIO_TO_REPORT(salary.salvalue) OVER (PARTITION BY career.jobno) ratio 
+    FROM career INNER JOIN salary on career.empno = salary.empno 
+      WHERE career.enddate IS NULL 
+      ORDER BY ratio DESC;
